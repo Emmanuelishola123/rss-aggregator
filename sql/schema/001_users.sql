887d6fcff3dac5207adcc98e01abaab88b21d7bf
emmanuelishola123 <emmanuelishola7@gmail.com>
2023-09-19 11:48:40 +0100

    UPDATE

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -4,10 +4,10 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-)
+    updated_at TIMESTAMP NOT NULL
+);
 
 
 -- +goose Down 
 
-DROP TABLE users
+DROP TABLE users;
